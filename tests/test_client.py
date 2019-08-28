6f82ab4a4e8167a8c9df9d5a26e9effbba3d71d2
Kayleigh <kayleigh@rix.run>
2019-08-28 14:31:12 +0000

    [add] test for expected errors
    If a filepath is passed and the file does not exit, expect an error to be raised.
    
    I would like to add more tests to confirm the code logic, tailling the file, adding data to log file while tailing the file, and integration tests with graphite, etc... Since my code relies on libraries I found that I would likely be testing libraries rather than my own code or testing my mocks. There is definitely space to include more tests but integration tests is what would matter more here if desired. Given this is just a simple script manual testing could be satisfactory for now.

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,23 +1,25 @@
 import os
 import pytest
-from graphite_client import get_parser, is_valid_file
+import graphite_client as gc
 
 
 class TestArgHandler(object):
 
     @classmethod
     def setup_class(cls):
-        cls.parser = get_parser()
-        cls.filename = "delete.log"
-        open(cls.filename, "w+").close()
-
+        cls.parser = gc.get_parser()
+        cls.filename = 'delete.log'
+        open(cls.filename, 'w+').close()
 
     @classmethod
     def teardown_class(cls):
         os.remove(cls.filename)
 
-
     def test_file_exists(self):
-        assert is_valid_file(self.parser, self.filename)
+        assert gc.is_valid_file(self.parser, self.filename)
 
+    def test_file_missing(self):
+        with pytest.raises(SystemExit) as exc:
+            gc.is_valid_file(self.parser, 'missing.log')
+        assert exc.value.code == 2
 
